0addd905bc8d4aa0e431348ffa7531128c71b439
ekalkur <kapil.k.kumar@ericsson.com>
2014-07-09 15:29:05 +0100

    Arquillian test modified for async method call

--- stateless/stateless-jee-testsuite/src/test/java/com/ericsson/trainings/jee6/ejb/HelloWorldTest.java
+++ stateless/stateless-jee-testsuite/src/test/java/com/ericsson/trainings/jee6/ejb/HelloWorldTest.java
@@ -3,10 +3,13 @@ package com.ericsson.trainings.jee6.ejb;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.concurrent.Future;
+
 import javax.ejb.EJB;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
@@ -41,6 +44,7 @@ public class HelloWorldTest {
 	private HelloWorldLocal helloWorldEJB;
 
 	@Test
+	@InSequence(1)
 	public void testSayHello() throws Exception {
 		logger.info("Calling HelloWorld EJB from Arquillian...");
 
@@ -49,5 +53,17 @@ public class HelloWorldTest {
 
 		assertNotNull(output);
 		assertEquals(expected, output);
+	}
+
+	@Test
+	@InSequence(2)
+	public void testSayHelloAsync() throws Exception {
+		logger.info("Calling HelloWorld EJB from Arquillian...");
+
+		final String expected = "Hello !!! Test";
+		final Future<String> future = helloWorldEJB.sayHelloAsync("Test");
+
+		assertNotNull(future);
+		assertEquals(expected, future.get());
 	}
 }
